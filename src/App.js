55b2f84c93d8acbf6a09da4caaa22dfc21602248
namminsub <tbvhdaos@naver.com>
2023-01-19 17:43:56 +0900

    darkmode-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header'; // default붙음
+import Main from './components/Main';
+import { DarkContext } from "./context/DarkContext";
+import { NameContext } from './context/NameContext'; //default없으면 중괄호 붙음(이름으로 내보내기)
+
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NameContext.Provider value="bbbbbb">
+      {/* 위 태그로 감싸주면 안에 있는 내용을 다 사용할수 있음*/}  
+      <DarkContext.Provider value ={{isDark, setIsDark}}>
+                           {/* isDark: isDark, setIsDark:setIsDark 줄여씀*/} 
+      <div className="App">
+        <Header/>
+        <Main/>
+        <Footer/>
+      </div>
+      </DarkContext.Provider>
+    </NameContext.Provider>
   );
 }
-
 export default App;
+
+
+/* import React from 'react';
+import SassComponent from './SassComponent';
+const App = () => {
+  return (
+    <SassComponent/>
+  );
+};
+
+export default App; */
